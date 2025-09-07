0eef76e2b68f318200b8bab5ced81a9e988c9b01
Camille MONNIER <monnier.camille06@gmail.com>
2025-09-06 20:44:18 -0400

    Message

--- LLM.py
+++ LLM.py
@@ -1,5 +1,5 @@
 import numpy as np
-text = "hello"
+text = "hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello hello"
 
 def build_vocab(text):
     # 1. caractères uniques
@@ -24,18 +24,57 @@ def decode(encoded, idx2char):
         text += idx2char[i]
     return text
 
-def matrice(encoded):
-    one_hot = np.ones((len(encoded), len(encoded)), dtype=np.float32)
-    for i, idx in enumerate(encoded):
-        one_hot[i, idx] += 1.0
-    return one_hot
+def matrice(encoded, vocab_size):
+    counts = np.zeros((vocab_size, vocab_size), dtype=np.float32)
+    for i in range(len(encoded) - 1):
+        c1 = encoded[i]
+        c2 = encoded[i + 1]
+        counts[c1, c2] += 1
+    # Lissage
+    counts += 1
+    # Normalisation
+    probs = counts / counts.sum(axis=1, keepdims=True)
+    return probs
+
+def generate_text(start_char, length, char2idx, idx2char, probs):
+    text = start_char
+    current_idx = char2idx[start_char]
+    
+    for _ in range(length):
+        # distribution pour le caractère courant
+        p = probs[current_idx]
+        
+        # tirer un caractère aléatoire selon p
+        next_idx = np.random.choice(len(p), p=p)
+        
+        # ajouter au texte
+        text += idx2char[next_idx]
+        
+        # mettre à jour
+        current_idx = next_idx
+    
+    return text
+
+def perplexity(text, char2idx, probs):
+    encoded = endode(text, char2idx)
+    log_prob = 0.0
+    for i in range(len(encoded) - 1):
+        c1 = encoded[i]
+        c2 = encoded[i + 1]
+        log_prob += np.log(probs[c1, c2])
+    n = len(encoded) - 1
+    perplexity = np.exp(-log_prob / n)
+    return perplexity
 
 vocab, char2idx, idx2char = build_vocab(text)
 encoded = endode(text, char2idx)
 decoded = decode(encoded, idx2char)
-one_hot_matrix = matrice(encoded)
+one_hot_matrix = matrice(encoded, len(vocab))
 
 print(char2idx)
 print(encoded)
 print(decoded)
 print(one_hot_matrix)
+
+print(generate_text("h", 4, char2idx, idx2char, one_hot_matrix))
+print(perplexity(text, char2idx, one_hot_matrix))
